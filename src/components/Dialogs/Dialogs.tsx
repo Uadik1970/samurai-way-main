97d06a0410d5d2150542c24c4ffcf46b61bca7bc
Vadim Timofeev <uadik1970@mail.ru>
2023-01-23 04:58:39 +0500

    video-21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from 'react'
+import s from './Dialogs.module.css'
 
 export const Dialogs: FC = () => {
     return (
-        <div>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Vlad</div>
+                <div className={s.dialog}>Lena</div>
+                <div className={s.dialog}>Max</div>
+                <div className={s.dialog}>Oleg</div>
+                <div className={s.dialog}>Viktor</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>yo</div>
+                <div className={s.message}>hello</div>
+                <div className={s.message}>world</div>
+                <div className={s.message}>!!!</div>
+            </div>
+        </div>
     )
 }
