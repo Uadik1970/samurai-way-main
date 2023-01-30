b1e5ad6d1d6d6ecad507f124b63595461c8c7969
Vadim Timofeev <uadik1970@mail.ru>
2023-01-30 16:52:42 +0500

    video-31

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,8 +6,8 @@ import { Message } from './Messages/Message'
 
 export const Dialogs: FC<DialogsPropsType> = ({ messages, dialogs }) => {
 
-    const dialogsElements = dialogs.map(d => <DialogItem id={d.id} name={d.name} />)
-    const messagesElements = messages.map(m => <Message id={m.id} message={m.message} />)
+    const dialogsElements = dialogs.map(d => <DialogItem key={d.id} id={d.id} name={d.name} />)
+    const messagesElements = messages.map(m => <Message key={m.id} id={m.id} message={m.message} />)
 
     return (
         <div className={s.dialogs}>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,21 +1,29 @@
-import React, { FC } from 'react'
+import React, { FC, LegacyRef } from 'react'
 import { Post } from './Post/Post'
 import s from './MyPosts.module.css'
 import { PostsPropsType } from '../Profile'
 
 export const MyPosts: FC<PostsPropsType> = ({ posts }) => {
 
-    const postsElements = posts.map(p => <Post id={p.id} message={p.message} likeCount={p.likeCount} />)
+    const postsElements = posts.map(p => <Post key={p.id} id={p.id} message={p.message} likeCount={p.likeCount} />)
+
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+
+
+    const addPost = () => {
+        let text = newPostElement.current?.value
+        alert(text)
+    }
 
     return (
         <div>
             <div>My posts</div>
             <div>
                 <div>
-                    <textarea name="" id=""></textarea>
+                    <textarea ref={newPostElement} name="" id=""></textarea>
                 </div>
                 <div>
-                    <button>add post</button></div>
+                    <button onClick={addPost}>add post</button></div>
             </div>
             <div className={s.posts}>
                 {postsElements}
